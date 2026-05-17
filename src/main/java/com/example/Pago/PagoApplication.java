335283e041470867342b12584c18e37ec1d8fbda
NachoSaave <b12josito@gmail.com>
2026-05-17 02:17:47 -0400

    idk

--- src/main/java/com/example/Pago/PagoApplication.java
+++ src/main/java/com/example/Pago/PagoApplication.java
@@ -9,5 +9,4 @@ public class PagoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PagoApplication.class, args);
 	}
-
 }
--- src/main/java/com/example/Pago/Repository/pagoRepository.java
+++ src/main/java/com/example/Pago/Repository/pagoRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.Pago.Model.Pago;
 
 public interface pagoRepository extends JpaRepository<Pago, Long> {
-
 }
